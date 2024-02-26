59d0f6f5c07ca476cca8e581e4914fedc6f76a9a
wu-wilson <wilson.cui.wu@gmail.com>
2024-02-25 19:30:07 -0500

    finished particle background component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import Particles from "./components/particles/Particles";
+import themes from "./_themes.module.scss";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Particles num={40} radius={6} color={themes["primary_color"]} />
+    </div>
+  );
 };
 
 export default App;
