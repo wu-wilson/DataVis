85778b647a3a647aa4a1853d674fd8e6190b4271
wu-wilson <wilson.cui.wu@gmail.com>
2024-04-21 14:40:59 -0400

    added first chart to comparison

--- src/pages/comparison/Comparison.tsx
+++ src/pages/comparison/Comparison.tsx
@@ -1,7 +1,48 @@
+import { useEffect, useState } from "react";
+import { parse } from "papaparse";
+import { ParsedRawData, isParsedRawData } from "../analysis/Analysis";
+import Chart4 from "./charts/Chart4/Chart4";
 import styles from "./Comparison.module.scss";
+import Loader from "../../components/loader/Loader";
 
 const Comparison = ({ year1, year2 }: { year1: number; year2: number }) => {
-  return <div className={styles["container"]}></div>;
+  const [loading, setLoading] = useState<boolean>(true);
+  const [parsedData, setParsedData] = useState<ParsedRawData | null>(null);
+
+  const parseData = async () => {
+    // Parse CSV data
+    const csv = await fetch("US_births_2000-2014_SSA.csv").then((res) =>
+      res.text()
+    );
+    const data = parse(csv, { header: true }).data;
+    if (isParsedRawData(data)) {
+      setParsedData(data);
+    }
+  };
+
+  useEffect(() => {
+    parseData();
+  }, []);
+
+  useEffect(() => {
+    if (parsedData) {
+      setLoading(false);
+    }
+  }, [parsedData]);
+
+  return loading ? (
+    <div className={styles["loader"]}>
+      <Loader message={"Getting your charts ready..."} />
+    </div>
+  ) : (
+    <div className={styles["container"]}>
+      {parsedData ? (
+        <div className={styles["charts"]}>
+          <Chart4 data={parsedData} year1={year1} year2={year2} />
+        </div>
+      ) : null}
+    </div>
+  );
 };
 
 export default Comparison;
---
+++ src/pages/comparison/charts/Chart4/util.ts
@@ -0,0 +1,150 @@
+import { ParsedRawData } from "../../../analysis/Analysis";
+import { SeriesLineOptions } from "highcharts";
+import themes from "../../../../_themes.module.scss";
+import styles from "./Chart4.module.scss";
+
+export const formatChart4Data = (
+  data: ParsedRawData,
+  year1: number,
+  year2: number
+): SeriesLineOptions[] => {
+  let formatted: SeriesLineOptions[] = [
+    { type: "line", color: themes.primary_color, name: "2000", data: [] },
+    { type: "line", color: "green", name: "2014", data: [] },
+  ];
+  // Map each month of the chosen years to its number of births
+  let counts: { [key: string]: number } = {};
+  data.forEach((row) => {
+    if (parseInt(row.year) === year1 || parseInt(row.year) === year2) {
+      const key = row.year + "," + (parseInt(row.month) - 1);
+      if (counts[key]) {
+        counts[key] += parseInt(row.births);
+      } else {
+        counts[key] = parseInt(row.births);
+      }
+    }
+  });
+  // Based on the map, fill the data array
+  for (const key in counts) {
+    const [year, month] = key.split(",");
+    const date: number = Date.UTC(2000, parseInt(month), 2);
+    if (parseInt(year) === year1) {
+      formatted[0].data?.push([date, counts[key]]);
+    } else {
+      formatted[1].data?.push([date, counts[key]]);
+    }
+  }
+  console.log(data);
+  return formatted;
+};
+
+const getTooltipTitle = (series: string, x: number) => {
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const date = new Date(x);
+  const monthIndex = date.getUTCMonth();
+  let title = `${series} ${months[monthIndex]}`;
+  return title;
+};
+
+export const chart4Options: Highcharts.Options = {
+  chart: {
+    type: "line",
+    zooming: {
+      type: "x",
+    },
+    style: {
+      fontFamily: themes.font_family,
+    },
+    backgroundColor: themes.chart_bg,
+    panning: {
+      enabled: true,
+      type: "x",
+    },
+    panKey: "shift",
+    events: {
+      selection: undefined,
+    },
+  },
+  title: {
+    style: {
+      color: themes.font_color,
+      fontWeight: "normal",
+      textDecoration: "underline",
+    },
+  },
+  legend: {
+    enabled: false,
+  },
+  credits: {
+    enabled: false,
+  },
+  accessibility: {
+    enabled: false,
+  },
+  xAxis: {
+    title: {
+      style: {
+        color: themes.font_color,
+      },
+    },
+    type: "datetime",
+    labels: {
+      format: "{value:%b.}",
+      style: {
+        color: themes.font_color,
+      },
+    },
+    tickColor: themes.font_color,
+    lineColor: themes.font_color,
+  },
+  yAxis: {
+    title: {
+      text: "Births",
+      style: {
+        color: themes.font_color,
+      },
+    },
+    labels: {
+      style: {
+        color: themes.font_color,
+      },
+    },
+    tickColor: themes.font_color,
+  },
+  plotOptions: {
+    series: {
+      animation: {
+        duration: 1500,
+      },
+    },
+  },
+  subtitle: {
+    text: `<span class=${styles["subtitle"]}><span>Drag over an area to zoom</span><span>Hold shift and drag to pan</span></span>`,
+    useHTML: true,
+  },
+  tooltip: {
+    useHTML: true,
+    formatter: function () {
+      return `<div class=${styles["tooltip"]}><span class=${
+        styles["title"]
+      }>${getTooltipTitle(
+        this.series.name,
+        this.point.x
+      )}</span><span>${this.point.y?.toLocaleString()} Births</span></div>`;
+    },
+  },
+  series: [],
+};
